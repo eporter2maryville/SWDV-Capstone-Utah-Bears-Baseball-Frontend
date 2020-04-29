707029fb864f9f53a8361fc1a3b87783adbeff3f
eporter2maryville <39113621+eporter2maryville@users.noreply.github.com>
2020-04-29 13:58:20 -0600

    Moved Header to JS

---
+++ app/app.js
@@ -0,0 +1,77 @@
+$(document).ready(function () {
+    console.log("Ready");
+    // app html
+    var app_html = `
+        <div class='container'>
+ 
+            <div class='page-header'>
+            <div class="container">
+            <div class="row">
+                <div class="col-sm-14 col-md-14 col-lg-14">
+    
+                    <img src="TBD" class="img-responsive" alt="Utah Bears Baseball Logo Banner"
+                        aria-label="Utah Bears Baseball Logo Banner">
+                    <div class="container">
+                        <div class="row" style="text-align:center">
+                            <div class="col-sm-12 col-md-12 col-lg-12">
+                                <div class="col-sm-2">
+                                    <h1><a href=home.html>Home</a></h1>
+                                </div>
+                                <div class="col-sm-2">
+                                    <h1><a href=Calendar.html>Calendar</a></h1>
+                                </div>
+                                <div class="col-sm-2">
+                                    <h1><a href=About.html>About the Team</a></h1>
+                                    </h1>
+                                </div>
+                                <div class="col-sm-2">
+                                    <h1><a href=Contact.html>Contact</a> </h1>
+                                </div>
+                                <div class="col-sm-2">
+                                    <h1><a href=Admin.html>Admin</a></h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            </div>
+ 
+            <!-- this is where the contents will be shown. -->
+            <div id='page-content'></div>
+ 
+        </div>`;
+
+    // inject to 'app' in home.html
+    $("#app").html(app_html);
+
+});
+ 
+// change page title
+function changePageTitle(page_title){
+ 
+    // change page title
+    $('#page-title').text(page_title);
+ 
+    // change title tag
+    document.title=page_title;
+}
+ 
+// function to make form values to json format
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
