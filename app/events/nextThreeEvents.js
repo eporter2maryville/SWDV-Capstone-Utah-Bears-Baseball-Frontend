4b2e17a036c6ea5304d1350bac6eae8343e84187
eporter2maryville <39113621+eporter2maryville@users.noreply.github.com>
2020-04-29 21:14:11 -0600

    Added next three events js service

---
+++ app/events/nextThreeEvents.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+ 
+    // show list of events on first load
+    showEvents();
+ 
+});
+ 
+// showevents() method
+function showEvents(){
+    //get list of events from the API
+    $.getJSON("http://utahbearsbaseball.net/api/events/readNextThree.php", function(response){
+
+    
+
+    // html for listing products
+var read_events_html=`
+<!-- start table -->
+<table class='table table-bordered table-hover'>
+ 
+    <!-- creating our table heading -->
+    <tr>
+        <th>Type</th>
+        <th>Date</th>
+        <th>Time</th>
+        <th>Location</th>
+        <th>Opponent</th>
+    </tr>`;
+    //rows go here 
+    // loop through returned list of data
+$.each(response.records, function(key, val) {
+ 
+    // creating new table row per record
+    read_events_html+=`
+        <tr>
+            <td>` + val.Type + `</td>
+            <td>` + val.Date + `</td>
+            <td>` + val.Time + `</td>
+            <td>` + val.Location + `</td>
+            <td>` + val.Opponent + `</td>
+        </tr>`;
+});
+ 
+// end table
+read_events_html+=`</table>`;
+// inject to 'page-content' of our app
+$("#next-three-event-content").html(read_events_html);
+});
+};
